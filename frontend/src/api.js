e50ce23c02394ff8b378a5cdb37e3a3c30765cb9
Sriyaan28 <sriyaanreddy4320@gmail.com>
2026-05-25 20:34:09 +0530

    fix vercel serverless functions

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://blog-app-theta-ruby.vercel.app";
+export const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://blog-app-theta-ruby.vercel.app/_/backend";
 //export const BACKEND_BASE_URL = "http://localhost:4000"
 
 export const api = axios.create({
